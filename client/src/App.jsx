bbb71adb7f95fe26e38f9d594476c671fe3578a8
yuzhiliu8 <yuzhiliu8@gmail.com>
2025-04-11 19:06:44 -0400

    init react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router'
+import LoginPage from './routes/LoginPage/LoginPage'
+import HomePage from './routes/HomePage/HomePage' 
+import RootRedirect from './routes/RootRedirect/RootRedirect'
 
 function App() {
 
     return (
         <div className="app">
-            app
+            <BrowserRouter> 
+                <Routes>
+                    <Route path="/" element={<RootRedirect />}/>
+                    <Route path="/login" element={<LoginPage />}/>
+                    <Route path="/home" element={<HomePage />}/>
+                </Routes>
+             
+            </BrowserRouter>
         </div>
     )
 }
