5d68d76c5b8a816ed491617e96160091fe5a6725
Andrewg314 <andrewguo108.dog@gmail.com>
2025-04-11 19:49:20 -0400

    login page update

--- client/src/routes/LoginPage/LoginPage.jsx
+++ client/src/routes/LoginPage/LoginPage.jsx
@@ -1,13 +1,38 @@
+import React from 'react'
 import './LoginPage.css'
 
-function LoginPage() {
-
-    return (
-        <div className="loginpage">
-            login page
-
+const LoginPage = () => {
+  return (
+    <div className="login-wrapper">
+      <div className="login-box">
+        <div className="logo">
+          <span>MacroMaster</span>
         </div>
-    )
-}
 
-export default LoginPage
+        <h2>Welcome back</h2>
+        <p>Please enter your details</p>
+
+        <form>
+          <input type="email" placeholder="Email address" required />
+          <input type="password" placeholder="Password" required />
+
+          <div className="options">
+            <label>
+              <input type="checkbox" />
+              Remember for 30 days
+            </label>
+            <a href="#">Forgot password</a>
+          </div>
+
+          <button type="submit">Sign in</button>
+        </form>
+
+        <div className="signup-link">
+          Don&apos;t have an account? <a href="#">Sign up</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
