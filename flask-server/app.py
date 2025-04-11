e4a32fadae4d21345dc172c7d5502f71ce1e6b6a
yuzhiliu8 <yuzhiliu8@gmail.com>
2025-04-11 12:39:02 -0400

    db init

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from dotenv import load_dotenv
+from db import db
+import os
 
+load_dotenv
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    conn_string = os.getenv("DATABSE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = conn_string
+    db.init_app(app)
 
-
+    return app
 
 if __name__ == "__main__":
+    load_dotenv()
+    app = create_app()
     app.run(port=5000, debug=True)
