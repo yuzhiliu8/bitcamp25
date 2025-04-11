430b3ce6703bcccde653b8a9dcf80a40dea12d8d
yuzhiliu8 <yuzhiliu8@gmail.com>
2025-04-11 13:53:15 -0400

    restart

--- client/src/routes/LoginPage.jsx
+++ client/src/routes/LoginPage.jsx
@@ -1,27 +1,9 @@
 import './LoginPage.css'
 
 function LoginPage() {
-    const localLoginData= {
-        username: "",
-        password: ""};
-
-    const handleChange = (event) => {
-        const {name, value}=event.target;
-        localLoginData[name]=value;
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setLoginData(localLoginData);
-    }
 
     return (
-        <form onSubmit={handleSubmit}>
-            Username:
-            <input type="text" name="username" placeholder='Username' onChange={handleChange}/>
-            Password:
-            <input type="text" name="password" placeholder='Password' onChange={handleChange}/>
-            <input type="submit" value="Login"/>
-        </form>
+        <></>
     )
 }
 
