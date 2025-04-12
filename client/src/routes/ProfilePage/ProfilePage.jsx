e19de95de34ce2cddb7a26f77193c922f52f59a9
Andrewg314 <andrewguo108.dog@gmail.com>
2025-04-12 15:56:48 -0400

    small update

--- client/src/routes/ProfilePage/ProfilePage.jsx
+++ client/src/routes/ProfilePage/ProfilePage.jsx
@@ -9,17 +9,18 @@ function ProfilePage() {
     calories: 0,
     carbs: 0,
     protein: 0,
-    fat: 0,
+    fat: 0
   });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     updateGoals(prev => ({ ...prev, [name]: value }));
+    console.log(goals);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+    // add stuff to make this work can either make it update on the spot or display a success message
   }
 
   const user = {
@@ -42,12 +43,43 @@ function ProfilePage() {
           <div className="account-value">{user.email}</div>
         </div>
 
-        <form>
-          {/* write each goal */}
+        <form onSubmit={handleSubmit} className="account-field">
+          <label>Calories:&nbsp;</label>
+          <input 
+          className="account-value"
+          type="calories"
+          placeholder={goals.calories}
+          name="calories"
+          onChange={handleChange}>
+          </input>
+          <label>Carbs:&nbsp;</label>
+          <input
+            className="account-value"
+            type="carbs"
+            placeholder={goals.carbs}
+            name="carbs"
+            onChange={handleChange}>
+          </input>
+          <label>Protein:&nbsp;</label>
+          <input
+            className="account-value"
+            type="protein"
+            placeholder={goals.protein}
+            name="protein"
+            onChange={handleChange}> 
+          </input>
+          <label>Fat:&nbsp;</label>
+          <input
+            className="account-value"
+            type="fat"
+            placeholder={goals.fat}
+            name="fat"
+            onChange={handleChange}>
+          </input>
+          <button type="submit">Update Goals</button>
         </form>
 
         <div className="button-group">
-          {/* add a button for update goals */}
           <button className="back-button" onClick={() => navigate("/home")}>
             Back to Home
           </button>
