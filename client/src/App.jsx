d76c2149b2e31b6e4bd9a920e36d9831493bcb3b
Andrewg314 <andrewguo108.dog@gmail.com>
2025-04-12 15:20:42 -0400

    deleted goalspage and old routes to goalspage+pastlogs from app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import LoginPage from './routes/LoginPage/LoginPage'
 import HomePage from './routes/HomePage/HomePage' 
-import PastLogs from './routes/PastLogs/PastLogs'
-import GoalsPage from './routes/GoalsPage/GoalsPage'
 import RootRedirect from './routes/RootRedirect/RootRedirect'
 import SignUpPage from './routes/SignUpPage/SignUpPage'
 import AccountPage from './routes/AccountPage/AccountPage'
@@ -18,8 +16,6 @@ function App() {
                     <Route path="/login" element={<LoginPage />}/>
                     <Route path="/home" element={<HomePage />}/>
                     <Route path="/signup" element={<SignUpPage />}/>
-                    <Route path="/pastlogs" element={<PastLogs />}/>
-                    <Route path="/goals" element={<GoalsPage />}/>
                     <Route path="/account" element={<AccountPage />}/>
 
                 </Routes>
