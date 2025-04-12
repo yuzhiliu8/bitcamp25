1dd5d2d15f398beab69099a355a01e3f32c64440
yuzhiliu8 <yuzhiliu8@gmail.com>
2025-04-12 14:45:07 -0400

    model updates

--- flask-server/model/model_controller.py
+++ flask-server/model/model_controller.py
@@ -1,15 +1,18 @@
 from flask import Blueprint, request, jsonify, make_response
 from model.yolov8 import Yolov8
+from dotenv import load_dotenv
 from PIL import Image
 import io
 import os
 
+load_dotenv()
 model_controller = Blueprint('model_controller',__name__)
 api_key = os.getenv("USDA_API_KEY")
 model_path = os.getenv("MODEL_PATH")
+print(model_path, api_key)
 model = Yolov8(model_path, api_key)
 
-@model_controller.route("/show-goal/<int:user_id>", methods = ["POST"])
+@model_controller.route("/show-goal", methods = ["POST"])
 def inference():
     if 'image' not in request.files:
         return jsonify({'error': 'No image provided'}), 400
