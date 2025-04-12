3ff43c914c83fabd2adeba17cf6afe0a52592cdf
kevinndongg <kevin.dong.haonan@gmail.com>
2025-04-12 13:41:36 -0400

    created inference method

--- flask-server/model/model_controller.py
+++ flask-server/model/model_controller.py
@@ -1,13 +1,21 @@
 from flask import Blueprint, request, jsonify, make_response
 from yolov8 import Yolov8
+from PIL import Image
+import io
 import os
 
 model_controller = Blueprint('model_controller',__name__)
 api_key = os.getenv("USDA_API_KEY")
 model_path = os.getenv("MODEL_PATH")
-model = Yolov8()
+model = Yolov8(model_path, api_key)
 
-@model_controller.route("/show-goal/<int:user_id>", methods = ["GET"])
+@model_controller.route("/show-goal/<int:user_id>", methods = ["POST"])
 def inference():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image provided'}), 400
     
+    file = request.files["image"]
+    image = Image.open(io.BytesIO(file.read()))
+    # get image somehow
+    # all_macros = model.inference(image)
     pass
