a0d2ca7a402c319bf26740365f68f6b12f475aa3
Andrewg314 <andrewguo108.dog@gmail.com>
2025-04-11 21:07:54 -0400

    small update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import LoginPage from './routes/LoginPage/LoginPage'
 import HomePage from './routes/HomePage/HomePage' 
 import RootRedirect from './routes/RootRedirect/RootRedirect'
+import { useState } from 'react'
 
 function App() {
+
+    const [loginData, setLoginData] = useState({
+        username:"",
+        password:"",
+    });
 
     return (
         <div className="app">
             <BrowserRouter> 
                 <Routes>
                     <Route path="/" element={<RootRedirect />}/>
-                    <Route path="/login" element={<LoginPage />}/>
+                    <Route path="/login" element={<LoginPage setLoginData={setLoginData} loginData={loginData} />}/>
                     <Route path="/home" element={<HomePage />}/>
                 </Routes>
              
