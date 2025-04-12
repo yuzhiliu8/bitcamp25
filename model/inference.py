1ae3c97160fe339e84a5b918d1848cbac84a4b22
kevinndongg <kevin.dong.haonan@gmail.com>
2025-04-12 01:44:33 -0400

    testing api test test

--- model/inference.py
+++ model/inference.py
@@ -1,17 +1,34 @@
 from ultralytics import YOLO
 from collections import Counter
 import cv2
+import requests
 
-# api_key = "JRFZ8fRJnKhVuqkU7DR9kkzfweTZJQaEaCwSrWZk"
+API_KEY = "JRFZ8fRJnKhVuqkU7DR9kkzfweTZJQaEaCwSrWZk"
 
 def main(): 
     # Load an Open Images Dataset V7 pretrained YOLOv8n model
     model = YOLO("yolov8x-oiv7.pt")
 
+    file = "model/inference-images/cucumbertomato.jpg"
+
     # Run prediction
     # run inference
-    results = model("model/inference-images/cucumbertomato.jpg", show = True, save = True)
+    results = model(file, show = True, save = True)
 
+    show_results(model, results)
+
+def search_food(query, api_key):
+    url = f"https://api.nal.usda.gov/fdc/v1/foods/search"
+    params = {
+        "query": query,
+        "pageSize": 1,
+        "api_key": api_key,
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data['foods'][0] if data['foods'] else None
+
+def show_results(model, results):
     # Counter to track class occurrences
     class_counter = Counter()
 
@@ -26,8 +43,14 @@ def main():
     print("✅ Detected Classes and Counts:")
     for class_name, count in class_counter.items():
         print(f"{class_name}: {count}")
-
-# def search_food(query, api_key):
-
+        food_item = search_food(class_name, API_KEY)
+        print(get_calories_per_gram(food_item=food_item))
+    
+def get_calories_per_gram(food_item):
+    for nutrient in food_item['foodNutrients']:
+        if 'energy' in nutrient['nutrientName'].lower() and nutrient['unitName'] == 'KCAL':
+            # usually given per 100g
+            return nutrient['value'] / 100
+    return None
 
 main()
