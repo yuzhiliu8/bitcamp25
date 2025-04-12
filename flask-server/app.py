af475a0c221ad5b8342dcd40d25136f5c56dc8d0
namebob <nathan07.chou@gmail.com>
2025-04-12 11:12:06 -0500

    goals

--- flask-server/app.py
+++ flask-server/app.py
@@ -5,16 +5,20 @@ from users.users import User
 import os
 from users.user_controller import user_controller
 from auth.auth_controller import auth_controller
+from goals.goals_controller import goals_controller
 
 from users.user_service import UserService
 from auth.auth_service import AuthService
 from cal_logs.cal_log_service import CalorieLogService
+from goals.goals_service import GoalsService
+
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(user_controller, url_prefix="/api/users")
     app.register_blueprint(auth_controller, url_prefix="/api/auth")
+    app.register_blueprint(goals_controller, url_prefix = "/api/goal")
 
     conn_string = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_DATABASE_URI"] = conn_string
@@ -29,8 +33,11 @@ def create_app():
             u = UserService()
             a = AuthService()
             c = CalorieLogService()
-
+            g = GoalsService()
+            
+            
             u.init_db()
+            g.init_db()
             a.init_db()
             c.init_db()
         else:
