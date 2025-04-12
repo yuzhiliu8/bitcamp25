6e23d1748db1eabd43baa640911ecb7553678755
yuzhiliu8 <yuzhiliu8@gmail.com>
2025-04-12 18:37:58 -0400

    root redirect functioning

--- client/src/routes/RootRedirect/RootRedirect.jsx
+++ client/src/routes/RootRedirect/RootRedirect.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { useNavigate } from 'react-router'
 import { useEffect } from 'react';
+import { API_URL } from '../../util/Constants';
 
 
 export default function RootRedirect() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        navigate('/login');
+        const authenticate = async () => {
+            const response = await fetch(`${API_URL}/api/auth/authenticate-session`, {
+                method: "GET",
+                credentials: "include",
+            });
+
+            if (response.status == 401){
+                navigate('/login');
+            }
+            else if (response.ok){
+                navigate('/home');
+            }
+        }
+
+        authenticate();
+        // navigate('/login');
     }, []);
 
 
